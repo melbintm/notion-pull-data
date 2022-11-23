5cfd5e8c0ee5e8663363bc1d0caf5c70a742d2ce
Melbin <melbin@softsquare.biz>
2022-11-23 13:17:17 +0530

    Added dynamic support fetch Notion database with Postgres tables

---
+++ src/db.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import { Client } from 'pg';
+dotenv.config();
+
+const client = new Client({
+    user: process.env.DB_USERNAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DBNAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    ssl: true,
+})
+
+export const connectDb =  (): any => {
+    return client.connect();
+};
+
+export const getNotionDbId = (dbName: string): any => {
+    return client.query('select id, db_id, name from dbList where name=$1', [dbName])
+        .then(result => {
+            if (result.rows && result.rows.length) {
+                return result.rows[0].db_id;
+            } else {
+                throw 'not found';
+            }
+        });
+}
